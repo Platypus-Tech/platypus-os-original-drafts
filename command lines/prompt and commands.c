ea16ce8be031b5b45d965eb34f318a4d17c9ef89
DFC <69485115+DamieFC@users.noreply.github.com>
2020-11-09 09:07:39 -0800

    Create prompt and commands.c
    And folder

---
+++ command lines/prompt and commands.c
@@ -0,0 +1,83 @@
+#import <stdio.h>	
+main(){
+	// important variables
+	main(){
+		char prompt = 'user@platypusos';
+		char loginPrompt = 'type "login" to login \n'
+		commandPrompt()
+		// this next bit will be ALL of the commands, with a variable or two or three
+	}
+	// command prompt
+	type commandPrompt() {
+		while(1) { // This will make it just put the command prompt on screen forever, which would be a majorly bad 					glitch. Must change ASAP, but not sure how YET. YET.
+			printf("%c \n", prompt);
+	}
+}
+	// Input function
+	// now THIS will be a LOT of the commands
+	type input() {
+		printf("%c \n", loginPrompt)
+		if // loginScan == done {
+			printf("%c \n", prompt)
+	}
+		else{
+			printf("%c \n", loginPrompt)
+	}
+		if 
+}
+	// input login thingamajigger
+	type LoginScan() {
+		char usernameInput;
+		char passwordInput;
+			if input == 'login' {				// never declared variable
+				char username = 'PTLV';		// Major
+				char password = 'Platypus';	// security
+				printf("Username: \n");
+				scanf("%s", usernameInput);
+				printf("Password: \n");
+				scanf("%s", passwordInput);
+					if usernameInput == username && passwordInput == password { // flaw, don't know how to fix it
+						commandPrompt();
+					}
+					else {
+						printf("Access Denied \n");
+						if input == 'login' {				// never declared variable
+						char username = 'PTLV';		// Major
+						char password = 'Platypus';	// security
+						printf("Username: \n");
+						scanf("%s", usernameInput);
+						printf("Password: \n");
+						scanf("%s", passwordInput);
+						if usernameInput == username && passwordInput == password { // flaw, don't know how to fix it
+							commandPrompt();
+						}
+						else {
+							printf("Access Denied \n");
+							if input == 'login' {				// never declared variable
+				char username = 'PTLV';		// Major
+				char password = 'Platypus';	// security
+				printf("Username: \n");
+				scanf("%s", usernameInput);
+				printf("Password: \n");
+				scanf("%s", passwordInput);
+					if usernameInput == username && passwordInput == password { // flaw, don't know how to fix it
+						commandPrompt();
+					}
+					else {
+						printf("Access Denied \n");
+						if input == 'login' {				// never declared variable
+						char username = 'PTLV';		// Major
+						char password = 'Platypus';	// security
+						printf("Username: \n");
+						scanf("%s", usernameInput);
+						printf("Password: \n");
+						scanf("%s", passwordInput);
+						if usernameInput == username && passwordInput == password { // flaw, don't know how to fix it
+							commandPrompt();
+						}
+						else {
+							printf("Access Denied \n");
+
+					}
+		}
+	}
