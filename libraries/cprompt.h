af7faeea69a3faeb86ee309650e8cf3af3083b2c
DFC <69485115+DamieFC@users.noreply.github.com>
2021-01-13 15:06:40 -0800

    Create cprompt.h

---
+++ libraries/cprompt.h
@@ -0,0 +1,23 @@
+#ifndef CPROMPT_H
+#define CPROMPT_H
+// Important libraries
+
+#include<stdio.h>
+#include<math.h>
+#include<limits.h>
+
+/* important variables */
+
+char input;
+
+// char fs = filesystem location;
+
+prompt = "you@platypusos:~#"
+
+void prscnPrompt();
+
+void prscnPrompt(){
+    printf("%s", prompt);
+    scanf("%s", input);
+}
+ENDIF CPROMPT_H
