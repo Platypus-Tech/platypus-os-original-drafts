e279b5d4ff7bc595930bc71014945f403720d389
DFC <69485115+DamieFC@users.noreply.github.com>
2021-01-29 10:19:57 -0800

    Create atomickittylitter.c

---
+++ command lines/platypusPrompt/eastereggs/atomickittylitter.c
@@ -0,0 +1,7 @@
+/*****************************************************
+ * This file (atomickittylitter.c) is licensed under *
+ * the GNU GPLv3. It (the file, not the license)     *
+ * displays a made-up ad advertising Atomic Kitty    *
+ * Litter.                                           *
+ *****************************************************/
+#include "../cprompt.h"
