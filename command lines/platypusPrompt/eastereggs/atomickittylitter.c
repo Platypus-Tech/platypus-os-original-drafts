12dbd759b69899de3d13a1849db61cf757f97999
DFC <69485115+DamieFC@users.noreply.github.com>
2021-01-29 10:42:10 -0800

    Update atomickittylitter.c
    Finally!

--- command lines/platypusPrompt/eastereggs/atomickittylitter.c
+++ command lines/platypusPrompt/eastereggs/atomickittylitter.c
@@ -4,6 +4,13 @@
  * displays a made-up ad advertising Atomic Kitty    *
  * Litter.                                           *
  *****************************************************/
-#include "../../libraries/cli/cprompt.h"
+#include "../../../libraries/cli/cprompt.h"
 
-prtscnPrompt();
+prscnPrompt();
+int main(){
+    if (input == "kittylitter"){
+        printf("Sick of cleaning the litterbox? This is the product for you! Atomic Kitty Litter makes it so you never have to clean it again! It kills the cat, so there's no problem! No cat no poop! But wait! There's more! Order now and you'll get 2 bags of Atomic Kitty Litter, PLUS the new Fusion Kitty Litter, which works almost immediatly! \n");
+        printf("\n Warning: can cause mass human death. Do not use in populated areas. You are responsible for obeying local nuclear material laws. \n");
+        printf("%s", prompt);
+    };
+};
