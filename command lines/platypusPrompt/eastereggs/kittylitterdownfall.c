2e94ba9f849bb216bc94e738c33b991b802f387c
DFC <69485115+DamieFC@users.noreply.github.com>
2021-02-01 14:36:26 -0800

    Create kittylitterdownfall.c

---
+++ command lines/platypusPrompt/eastereggs/kittylitterdownfall.c
@@ -0,0 +1,11 @@
+#include "../../libraries/cli/cprompt.h"
+
+int main(){
+	prscnPrompt();
+	if (input == "downtolitter" || "kittylitterdownfall"){
+		printf("BREAKING NEWS-ATOMIC KITTY LITTER OWNER ARRESTED, CHARGED WITH UNLAWFUL POSSESION OF NUCLEAR WEAPONS. \n");
+		printf("-------------------------------------------------------------------- \n")
+		printf("In an amazing new update, the owner of Atomic Kitty Litter was arrested for unlawful possesion of Nuclear Weapons. Atomic Kitty Litter is a product, which is advertised as a product that will get rid of cleaning cat poo forever. But, in an investigation involving using a mass spectrometer, particle accelerator, and reading the ingredients label (in that order), it has been exposed that the active ingredient is an element found in atomic bomb blasts. We cannot tell you (it's Einsteinium and Fermium and Plutonium and Radium), because of nuclear warfare fears. \n");
+		printf("%s", prompt);
+	}
+}
