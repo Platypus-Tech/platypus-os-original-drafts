9acbe72659a9ae909c2bac453e7bf64ee23c2e0d
DFC <69485115+DamieFC@users.noreply.github.com>
2021-02-01 09:35:32 -0800

    Update atomickittylitter.c

--- command lines/platypusPrompt/eastereggs/atomickittylitter.c
+++ command lines/platypusPrompt/eastereggs/atomickittylitter.c
@@ -9,7 +9,9 @@
 prscnPrompt();
 int main(){
     if (input == "kittylitter"){
-        printf("Sick of cleaning the litterbox? This is the product for you! Atomic Kitty Litter makes it so you never have to clean it again! It kills the cat, so there's no problem! No cat no poop! But wait! There's more! Order now and you'll get 2 bags of Atomic Kitty Litter, PLUS the new Fusion Kitty Litter, which works almost immediatly! \n");
+        printf("Sick of cleaning the litterbox? This is the product for you! Atomic Kitty Litter makes it so you never have to clean it again! \n"); 
+        printf("It kills the cat, so there's no problem! No cat no poop! \n");
+        printf("But wait! There's more! Order now and you'll get 2 bags of Atomic Kitty Litter, PLUS the new Fusion Kitty Litter, which works almost immediatly! \n");
         printf("\n Warning: can cause mass human death. Do not use in populated areas. You are responsible for obeying local nuclear material laws. \n");
         printf("%s", prompt);
     };
