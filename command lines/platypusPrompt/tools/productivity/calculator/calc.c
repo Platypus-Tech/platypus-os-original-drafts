928f41d1ab5cf08c4c997957f6e95cbab2325f3a
DFC <69485115+DamieFC@users.noreply.github.com>
2021-02-02 11:24:39 -0800

    Create calc.c
    Note to self, change

---
+++ command lines/platypusPrompt/tools/productivity/calculator/calc.c
@@ -0,0 +1,6 @@
+/**************************************************************
+ * This file, calc.c, is licensed under the GNU GPLv3 License *
+ * (c) DamieFC 2021                                           *
+ **************************************************************/
+ 
+ #include "/* whatever it was called */"
