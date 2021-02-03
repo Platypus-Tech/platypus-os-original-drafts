6add3574c4b1153dead52028658fbd6f93fe5edd
DFC <69485115+DamieFC@users.noreply.github.com>
2021-02-02 19:02:32 -0800

    Create logininfo.h

---
+++ security/logininfo.h
@@ -0,0 +1,14 @@
+/********************************************************************
+ * This file, logininfo.h, is made for the login.c file for logging *
+ * in. It's licensed under the GNU GPL version 3.                   *
+ * (c) DamieFC, 2021                                                *
+ ********************************************************************/
+#include "../libraries/cli/cprompt.h"
+
+// usernames and passwords
+// root users stuff
+#define rootusername "root"
+#define rootpass "Pl@7ypu505"
+// normal user's stuff
+#define normusername "you"
+#define normpass "Pl@7ypu5u53r"
