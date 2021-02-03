d6885e14ff0996372f1c232f5d48b42d9d6ea75a
DFC <69485115+DamieFC@users.noreply.github.com>
2021-02-02 18:50:08 -0800

    Update login.c
    I hope the previous is in archives program 'cause it's gone bye bye.

--- security/login.c
+++ security/login.c
@@ -1,73 +1,15 @@
-/#include <stdio.h>
-/*********************************************************
- * All of this code came from the file					 *
- * prompt and commands.c. Created by Damien Chambliss	 *
- * GNU General Public License v3.0						 *
- *********************************************************/
-type LoginScan() {
-	char usernameInput;
-	char passwordInput;
-		if input == 'login' {				// never declared variable
-			char username = 'PTLV';		// Major
-			char password = 'Platypus';	// security
-			printf("Username: \n");
-			scanf("%s", usernameInput);
-			printf("Password: \n");
-			scanf("%s", passwordInput);
-				if usernameInput == username && passwordInput == password { // flaw, don't know how to fix it
-					commandPrompt();
-				}
-				else {
-					printf("Access Denied \n");
-					if input == 'login' {				// never declared variable
-					char username = 'PTLV';		// Major
-					char password = 'Platypus';	// security
-					printf("Username: \n");
-					scanf("%s", usernameInput);
-					printf("Password: \n");
-					scanf("%s", passwordInput);
-					if usernameInput == username && passwordInput == password { // flaw, don't know how to fix it
-						commandPrompt();
-					}
-					else {
-						printf("Access Denied \n");
-							if input == 'login' {				// never declared variable
-								char username = 'PTLV';		// Major
-								char password = 'Platypus';	// security
-					printf("Username: \n");
-					scanf("%s", usernameInput);
-					printf("Password: \n");
-					scanf("%s", passwordInput);
-						if usernameInput == username && passwordInput == password { // flaw, don't know how to fix it
-							commandPrompt();
-						}
-						else {
-							printf("Access Denied \n");
-							if input == 'login' {				// never declared variable
-							char username = 'PTLV';		// Major
-							char password = 'Platypus';	// security
-							printf("Username: \n");
-							scanf("%s", usernameInput);
-							printf("Password: \n");
-							scanf("%s", passwordInput);
-							if usernameInput == username && passwordInput == password { // flaw, don't know how to fix it
-								commandPrompt();
-							}
-							else {
-								printf("Access Denied \n");
+/********************************************************
+ * This file, login.c, is licensed under the GNU GPLv3. *
+ * (c) DamieFC 2021										*
+ ********************************************************/
+#include "../libraries/cli/cprompt.h"
+#include <stdio.h>
 
-						}
-			}
-		}
+int main(){
+	char loginprompt = "login"
+	printf("loginprompt");
+	scanf("%s", input)
+	if (input == "login"){
+		// I think I'll need another .h file. Fudge.
 	}
 }
-}
-}
-}
-}
-/***************************************************************************
- * Licensed under GNU General Public License v3.0						               *
- * Created by Damien Chambliss, benevolent dictator of the Platypus Kernel *
- * Platypus Tech 2020													                             *
- * Login.c file, for logging in!										                       *
- ***************************************************************************
