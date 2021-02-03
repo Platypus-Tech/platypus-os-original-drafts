9cef53b90a45aec489038158a2b38e22c54964ea
DFC <69485115+DamieFC@users.noreply.github.com>
2021-02-03 09:43:30 -0800

    Update login.c
    Got the login thing for root. Now if I could unconfuse myself with all of these curly brackets, that would be GREAT.

--- security/login.c
+++ security/login.c
@@ -4,13 +4,23 @@
  ********************************************************/
 #include "../libraries/cli/cprompt.h"
 #include "logininfo.h"
-#include <stdio.h>
 
 int main(){
 	char loginprompt = "Type \"login\" to login: "
 	printf("loginprompt");
-	scanf("%s", input)
+	scanf("%s", input);
 	if (input == "login"){
-		// I think I'll need another .h file. Fudge.
+		// I got another .h file! AND WE'RE OFF LIKE A HERD OF TURTLES!
+		char attemptusername
+		char attemptpassword
+		scanf("%s", attemptusername);
+		if(attemptusername == rootusername){
+			printf("Ok, root! Password: ");
+			scanf("%s", attemptpassword);
+			if (attemptpassword == rootpass){
+				printf("Hi, root! \n");
+				printf("%s", prompt);
+			}
+		}
 	}
 }
