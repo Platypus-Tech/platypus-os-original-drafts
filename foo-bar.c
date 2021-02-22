618de1cd6f6ed3b51d2524ec8bf5925c7822cecf
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-02-21 19:50:32 -0800

    Update and rename foo.c to foo-bar.c
    Sorry if you're not straight edge.

--- foo-bar.c
+++ foo-bar.c
@@ -1,12 +1,12 @@
 /******************************************************************************************
  * This program is just for fun as an example. It's also licensed under the GNU GPL v3.0. *
- * It is questioning the term "foo" for an example term.                                  *
+ * It is questioning the term "foo-bar" for an example term.                                  *
  ******************************************************************************************/
 
 #include <stdio.h>
 
 int main(){
 	int foo(){
-		printf("What does foo mean? It makes no sense. I guess it means example, so why isn't it example, like \"example.c\"? C\'mon, now. Gimme a break.");
+		printf("What does foo mean? It makes no sense. I guess it means example, so why isn't it example, like \"example.c\"? C\'mon, now. Gimme a break. I think a bar is where non-straight-edge people go.");
 	}
 }
