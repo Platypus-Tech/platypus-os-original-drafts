cc0fb1f134173137dee165706a7c046c5e753873
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-03-13 12:21:15 -0800

    Update foo-bar.c

--- foo-bar.c
+++ foo-bar.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 int main(){
-	int foo(){
+	int foo(void){
 		printf("What does foo mean? It makes no sense. I guess it means example, so why isn't it example, like \"example.c\"? C\'mon, now. Gimme a break. I think a bar is where non-straight-edge people go.");
 	}
 }
