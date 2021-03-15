02b466bf35dd1850e89ce484481ce728c27eb03a
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-03-15 14:46:49 -0700

    Update hello.c

--- command lines/platypusPrompt/hello.c
+++ command lines/platypusPrompt/hello.c
@@ -1,7 +1,9 @@
-#include 'cprompt.h'
+#include '../../include/cli/cprompt.h'
 
 int main(){
+    prscnPrompt();
     if (input == "hello" || "Hello" || "HeLlO"){
         printf("Hi! Glad you're here!");
+        printf("%s", prompt)
     }
 }
