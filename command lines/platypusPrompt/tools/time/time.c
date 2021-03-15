5281c21d2021f37125f0b7e57e8dd7e043c50d16
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-03-15 13:44:13 -0700

    Create time.c
    Oh boy this is gonna be a mess

---
+++ command lines/platypusPrompt/tools/time/time.c
@@ -0,0 +1,39 @@
+/******************************************************************************************
+ * This file, time.c, is licensed under the GNU GPL version 3. It implements the `time`   *
+ * command in the OS. Honestly, the code is adapted from Wikipedia's example.             *
+ ******************************************************************************************/
+
+#include "../../../../include/common/time.h"
+#include "../../../../include/cli/prompt.h"
+#include <stdlib.h>
+
+int main(void)
+{
+    prscnPrompt();
+    if (input == "time" || "Time" || "TIME" || "tIME"){
+
+    time_t current_time;
+    char* c_time_string;
+
+    /* Obtain current time. */
+    current_time = time(NULL);
+
+    if (current_time == ((time_t)-1))
+    {
+        (void) fprintf(stderr, "Failure to obtain the current time.\n");
+        printf("%s", prompt);
+    }
+
+    /* Convert to local time format. */
+    c_time_string = ctime(&current_time);
+
+    if (c_time_string == NULL)
+    {
+        (void) fprintf(stderr, "Failure to convert the current time.\n");
+        printf("%s", prompt);
+    }
+
+    /* Print to stdout. ctime() has already added a terminating newline character. */
+    (void) printf("Current time is %s", c_time_string);
+    }
+}
