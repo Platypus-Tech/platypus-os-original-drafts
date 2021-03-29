6861e8564d2e3146333ca98efcb2545ba8a5f3b2
GNUOverYooouu <dtortoise99@gmail.com>
2021-03-29 10:26:29 -0700

    Update selfdestruct.c

--- kernel/selfdestruct.c
+++ kernel/selfdestruct.c
@@ -2,17 +2,17 @@
  * This file, selfdestruct.c is licensed under the GNU GPLv3.    *
  * (c) DamieFC, 2021                                             *
  *****************************************************************/
-#include "../libraries/cli/cprompt.h"
+#include "../libraries/cli/prompt.h"
 
 int main(){
-	if (input == "selfdestruct"){
+	if (input == "self-destruct"){
 		char sure
 		printf("Are you sure? This action cannot be undone. Y/n \n");
-		scanf("%c", sure)
+		gets("%c", sure)
 		if (sure == "y" || "Y" || "n" || "N"){
 			char extrasure
 			printf("Are you extra sure? Y/n \n");
-			scanf("%c", extrasure)
+			gets("%c", extrasure)
 			if (extrasure == "Y" || "y" || "N" || "n"){
 				printf("Ok... \n");
 				printf("Self destructing... \n");
