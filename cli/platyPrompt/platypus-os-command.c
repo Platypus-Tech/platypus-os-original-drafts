0cfbef9ebf4e842d623f1e9253112b5853e63e41
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-01 19:32:45 -0700

    Update platypus-os-command.c

--- cli/platyPrompt/platypus-os-command.c
+++ cli/platyPrompt/platypus-os-command.c
@@ -8,28 +8,22 @@
  * good idea to put that here, because the BASH shell source does.                   *
  *************************************************************************************/
 // The always important "stdio.h" header file
-#include <stdio.h>
+#include "../../include/cli/prompt.h"
 int main(){
-// A few important variables
-	char input // for when we are recieving input, notice no value
-	char cprompt = "you@platypusOS" // I have to create a file or something so I don't always have to put 	this
-	// On to the fun stuff!
-	printf("%s", cprompt) // Prints the command prompt on screen
-	scanf("%s", input) // stores what the user put as the variable "input" value
-	// now, to the classic if/else bit
+    prscnPrompt();
 	if (input == "platypusOS" || "Platypus OS" || "PlatypusOS" || "PLATYPUSOS" || "PlAtYpUsOs" || "pLaTyPuS oS" || "pLATYPUS os" || "pLATYPUSos" || "Platypus-OS" || "platypus-OS"){     /* it's good to have 	flexibility, but there's only so much you can provide. Other flexibility is appreciated.*/
 	/*	printf("PPPPPPPPPPPPb       LL                     :::         TTTTTTTTTTTT          YYY             	YYY       PPPPPPPPPPPPb        UU            UU        5555555555555             OOOOOOO          	SSSSSSSSSS /n")
 	printf("PP          P       LL                    AAAAA             TT                YYY           YYY         PP          P         UU          UU          55                      OO     OO          SS /n") I'm 	trying to write Platypus OS in ASCII. */
-		printf("PLATYPUS OS \n")
-		printf("Add the -v switch to show the version. \n")
-		printf("%s", cprompt)
+		printf("PLATYPUS OS \n");
+		printf("Add the -v switch to show the version. \n");
+		printf("%s", prompt);
 		if (input == "platypusOS -v" || "Platypus OS -v" || "PlatypusOS -v" || "PLATYPUSOS -v" || "PlAtYpUsOs -v" || "pLaTyPuS oS -v" || "pLATYPUS os -v" || "pLATYPUSos -v" || "Platypus-OS -v" || "platypus-OS -v"){
-			printf("Pre-alpha -0.9.6. In very very very early stages of development. \n")
-			printf("%s", cprompt)
+			printf("Pre-alpha -0.9.6. In very very very early stages of development. \n");
+			printf("%s", prompt);
 		}
 		else{
-			printf("Wut? \n")
-			printf("%s", cprompt)
+			printf("Wut? \n");
+			printf("%s", prompt);
 		}
 	}
 }
