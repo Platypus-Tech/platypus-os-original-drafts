2e5eb293361659b291be760d29602f1cdb2d65eb
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-01 19:33:47 -0700

    Update help.c

--- cli/platyPrompt/help.c
+++ cli/platyPrompt/help.c
@@ -2,20 +2,20 @@
  * This is the file "help.c" for the builtin command in the 	 *
  * Platypus OS shell, "help".									 *
  *****************************************************************/
-#include "cprompt.h"
+#include "prompt.h"
 // A few important variables
 if (input == "help" || "HELP" || "Help" || "HeLp" || "hELP" || "hElP" || "help!" || "Help!" || "HELP!" || "hELP!" || "HeLp!" || "hElP"){
 	printf("This is the Platypus OS command line. There are a few things that you need to know: \n");
 	printf("1. You can login using the 'login' command \n");
 	printf("2. This is still in development, but it is still usable \n");
 	printf("3. You can get the version of this by adding '-v' at the end of the command \n");
-	printf("%c", cprompt);
+	printf("%s", prompt);
 }
 else{
 		printf("huh? \n");
-		printf("%c", cprompt);
+		printf("%s", prompt);
 }
 if (input =="help -v" || "HELP -v" || "Help -v" || "HeLp -v" || "hELP -v" || "hElP -v" || "help! -v" || "Help! -v" || "HELP! -v" || "hELP! -v" || "HeLp! -v" || "hElP -v"){
-	printf("New \n");
-	printf("%c", cprompt);
+	printf("-0.9.4-rc2 \n");
+	printf("%s", prompt);
 }
