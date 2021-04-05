377327f448db720eb56a8dd9ba41131c6d6defa7
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-05 11:27:58 -0700

    Update hello.c

--- cli/platyPrompt/hello.c
+++ cli/platyPrompt/hello.c
@@ -1,9 +1,8 @@
 #include '../../include/cli/prompt.h'
 
 int main(){
-    prscnPrompt();
     if (input == "hello" || "Hello" || "HeLlO"){
         printf("Hi! Glad you're here!");
-        printf("%s", prompt);
+        prscnPrompt();
     }
 }
