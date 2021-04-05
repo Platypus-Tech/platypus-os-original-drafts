25ccf47d5716c7179b6d0ba104e5337d74973dd6
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-04 20:22:16 -0700

    Update and rename charkeymap.h to keymap.h

--- devices/my-drivers/keyboard/keymap.h
+++ devices/my-drivers/keyboard/keymap.h
@@ -3,9 +3,10 @@
  * (c) DamieFC 2021                                                     *
  ************************************************************************/
 
-char keys_char[48] = {
+char keys_char[48] = { /* essentially my ideal keyboard, except no shift keys, or control keys. AAAAAAAAA IT'S NOT MY PERFECT KEYBOARD I DON'T WANT THIS KEYBOARD */
     "`","1","2","3","4","5","6","7","8","9","0","-","=","\0"
     "\t","q","w","e","r","t","y","u","i","o","p","[","]","\\",
     "a","s","d","f","g","h","j","k","l",";","'",
-    "z","x","c","v","b","n","m",",",".","/"
+    "z","x","c","v","b","n","m",",",".","/",
+    " "
 };
