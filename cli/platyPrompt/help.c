a4f9b1d12567965172ee06b41c7c911d9781058a
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-05 11:27:27 -0700

    Update help.c

--- cli/platyPrompt/help.c
+++ cli/platyPrompt/help.c
@@ -2,7 +2,7 @@
  * This is the file "help.c" for the builtin command in the 	 *
  * Platypus OS shell, "help".									 *
  *****************************************************************/
-#include "prompt.h"
+#include "../../include/cli/prompt.h"
 // A few important variables
 if (input == "help" || "HELP" || "Help" || "HeLp" || "hELP" || "hElP" || "help!" || "Help!" || "HELP!" || "hELP!" || "HeLp!" || "hElP"){
 	printf("This is the Platypus OS command line. There are a few things that you need to know: \n");
@@ -13,7 +13,7 @@ if (input == "help" || "HELP" || "Help" || "HeLp" || "hELP" || "hElP" || "help!"
 }
 else{
 		printf("huh? \n");
-		printf("%s", prompt);
+		prscnPrompt();
 }
 if (input =="help -v" || "HELP -v" || "Help -v" || "HeLp -v" || "hELP -v" || "hElP -v" || "help! -v" || "Help! -v" || "HELP! -v" || "hELP! -v" || "HeLp! -v" || "hElP -v"){
 	printf("-0.9.4-rc2 \n");
