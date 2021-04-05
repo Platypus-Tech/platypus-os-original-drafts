7ad1d22ece0274071bef142d024f648312371b45
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-04 20:41:22 -0700

    Oops wrong escape sequence thingamabob

--- devices/my-drivers/keyboard/keymap.h
+++ devices/my-drivers/keyboard/keymap.h
@@ -4,7 +4,7 @@
  ************************************************************************/
 
 char keys_char[48] = { /* essentially my ideal keyboard, except no shift keys, or control keys. AAAAAAAAA IT'S NOT MY PERFECT KEYBOARD I DON'T WANT THIS KEYBOARD */
-    "`","1","2","3","4","5","6","7","8","9","0","-","=","\0"
+    "`","1","2","3","4","5","6","7","8","9","0","-","=","\b"
     "\t","q","w","e","r","t","y","u","i","o","p","[","]","\\",
     "a","s","d","f","g","h","j","k","l",";","'",
     "z","x","c","v","b","n","m",",",".","/",
