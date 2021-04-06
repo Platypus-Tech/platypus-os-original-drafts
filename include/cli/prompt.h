47ad246f6d65bb07bf4c603c39602c6ae4911e27
berg95 <52430539+berg95@users.noreply.github.com>
2021-04-05 22:23:26 -0700

    working directory in prompt
    Adds viewing current working directory as part of the prompt.

--- include/cli/prompt.h
+++ include/cli/prompt.h
@@ -2,22 +2,32 @@
 #define PROMPT_H
 // Important libraries
 
-#include<stdio.h>
-#include<math.h>
-#include<limits.h>
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* important variables */
 
+FILE *getpwd;
+char *command = "pwd";
+char c = 0;
+char str[];
 char input;
 
 // char fs = filesystem location;
 
-prompt = "you@platypusos:~#";
+getpwd = (FILE*)popen(command, "r");
+while (fread(&c, sizeof c, 1, getpwd)) {
+    strncat(str, &c, 1);
+}
+prompt = "you@platypusos:";
 
 void prscnPrompt();
 
 void prscnPrompt(){
-    printf("%s", prompt);
+    printf("%s%s#", prompt, str);
     gets("%s", input);
 }
 #ENDIF /* PROMPT_H */
