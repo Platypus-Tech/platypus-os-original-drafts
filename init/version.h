065fb4badd7c1e5d5db56e5f7e014a3485c51e9c
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-06 12:10:03 -0700

    Add version.h for main.c

---
+++ init/version.h
@@ -0,0 +1,6 @@
+/*************************************************************************************
+ * This file, version.h, is licensed under the GNU General Public License version 3. *
+ * This code is used in main.c for printing the version.                             *
+ *************************************************************************************/
+
+char version = "0.9.2-rc2";
